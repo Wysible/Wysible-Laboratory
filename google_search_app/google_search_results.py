82e22569c74e116c28c69e125009a82aba60495b
Nishanth <rajkumarnishanth@gmail.com>
2022-04-02 23:20:46 +0530

    feat: get top 3 results

--- google_search_app/google_search_results.py
+++ google_search_app/google_search_results.py
@@ -7,7 +7,7 @@
 '''
 from googlesearch import search
 
-def search_result(search_string: str):
+def search_result(search_string: str) -> list:
     """
         Description:
             Retieves search results for the search string
@@ -16,11 +16,16 @@ def search_result(search_string: str):
             Search string as str
         
         Return:
-            None
+            Top 3 results as list
     """
-    for j in search(search_string, tld="com", num=10, stop=5, pause=1):
+    links = []
+    for j in search(search_string, tld="com", num=10, stop=5, pause=0.0005):
         print(j)
-
+        links.append(j)
+    
+    print("Top 3 links:")
+    print(links[0]+'\n'+links[1]+'\n'+links[2])
+    return links[0:3]
 
 if __name__ == '__main__':
     search_result("lucene")
